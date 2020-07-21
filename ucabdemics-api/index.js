42eb89bed1083c53c023e2e8f995c282ba8e760a
Luis <luifersr@gmail.com>
2020-07-20 21:20:38 -0400

    Mongo access & CRUD tested

--- ucabdemics-api/index.js
+++ ucabdemics-api/index.js
@@ -1,8 +1,14 @@
 const app = require('express')();
+const bodyParser = require("body-parser")
 const { config } = require('./config/index');
+const api = require('./routes/server')
 
 app.use(require('express').json());
 app.use(require('cors')());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//api(app);
 
 app.listen(config.port, function () {
   console.log(`Listening on port ${config.port}`);
