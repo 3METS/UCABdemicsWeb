e92b6d06cef63610b75bddc2943182c69d7d7e1d
Tania Riobueno <tdgutierrez.17@est.ucab.edu.ve>
2020-07-23 07:04:27 -0400

    Agregando middlewares al index y modificando dependencias

--- ucabdemics-api/index.js
+++ ucabdemics-api/index.js
@@ -1,14 +1,27 @@
 const app = require('express')();
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser');
 const { config } = require('./config/index');
-const api = require('./routes/server')
+
+const {
+  logErrors,
+  errorHandler,
+  wrapError,
+} = require('./utils/middlewares/errorHandlers');
+
+const { notFoundHandler } = require('./utils/middlewares/notFoundHandler');
 
 app.use(require('express').json());
 app.use(require('cors')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//api(app);
+//Direcciona a 404 not found
+app.use(notFoundHandler);
+
+//Manejadores de Error
+app.use(logErrors);
+app.use(wrapError);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening on port ${config.port}`);
