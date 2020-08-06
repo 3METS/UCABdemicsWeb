b39215710245407b6c0082bb026c99ac5e4136ec
Tania Riobueno <tdgutierrez.17@est.ucab.edu.ve>
2020-08-05 22:52:17 -0400

    Servicios de envio de Emails

--- ucabdemics-api/services/mail.js
+++ ucabdemics-api/services/mail.js
@@ -1,64 +1,28 @@
-const { config } = require('../config/index');
-const nodemailer = require('nodemailer');
+const BullService = require('./bull');
 
 class MailService {
   constructor() {
-    this.transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: config.email,
-        pass: config.emailPassword,
-      },
-    });
+    this.bullService = new BullService('email');
   }
 
   getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  sendWelcome({ user }) {
-    var mailOptions = {
-      from: `"UCABdemics Team" <${config.email}>`,
-      to: user,
-      subject: '¡Bienvenido a UCABdemics!',
-      text: `<h1>¡Bienvenido a UCABdemics!</h1>
-      <p>Gracias por registrarse en nuestra aplicación</p>
-      `,
-    };
-
-    return new Promise((resolve, reject) => {
-      this.transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          reject(error);
-          return false;
-        } else {
-          console.log('Email sent: ' + info.response);
-          resolve(true);
-        }
-      });
+  sendWelcome({ email, res }) {
+    return this.bullService.addJobs({
+      data: { email, option: 'welcome' },
+      res,
     });
   }
 
-  sendRecovery({ user }) {
+  sendRecovery({ name, email, res }) {
     var recoveryCode = this.getRndInteger(100000, 999999);
-    var mailOptions = {
-      from: `"UCABdemics Team" <${config.email}>`,
-      to: user,
-      subject: 'Recuperación de contraseña',
-      text: `Su codigo de recuperacion: ${recoveryCode}`,
-    };
-
-    return new Promise((resolve, reject) => {
-      this.transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          reject(error);
-          return false;
-        } else {
-          console.log('Email sent: ' + info.response);
-          resolve(recoveryCode);
-        }
-      });
+    this.bullService.addJobs({
+      data: { name, email, recoveryCode, option: 'recovery' },
+      res,
     });
+    return recoveryCode;
   }
 }
 
