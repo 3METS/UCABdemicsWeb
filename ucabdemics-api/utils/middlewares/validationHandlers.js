8be73b652a130f48efd70bec97434518d972cc3d
Tania Riobueno <tdgutierrez.17@est.ucab.edu.ve>
2020-08-14 08:50:31 -0400

    Arreglando errores en el archivo ValidationHandlers

--- ucabdemics-api/utils/middlewares/validationHandlers.js
+++ ucabdemics-api/utils/middlewares/validationHandlers.js
@@ -7,7 +7,9 @@ function validate(data, schema) {
 
 function validationHandler(schema, check = 'body') {
   return function (req, res, next) {
-    const error = validate(req[check], schema);
+    const error = req[check].id
+      ? validate(req[check].id, schema)
+      : validate(req[check], schema);
     error ? next(boom.badRequest(error)) : next();
   };
 }
